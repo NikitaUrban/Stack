2160246fd2c851bc82dfe1391cae5b7ae561cb4f
nikitka <nick.urbanovich@mail.ru>
2020-07-28 14:41:05 +0300

    h

--- script.js
+++ script.js
@@ -1,37 +1,43 @@
 let stack = []
-pushElementOnStack = () => { // добавляем элементы в стек
-    let addElements=document.querySelector('#btn1')
-    addElements.addEventListener('click',function () {
-        try {
-            let N = prompt('Сколько элементов вы хотите добавить в стек:','')
-            if ( ! ( Number(N) ) ) { throw new Error('Ошибка ввода') }
-            for (let i = 0 ; i < N; i++){
-                let elem = parseFloat(prompt('Элемент ' + (i+1) + ':',''))
-                if ( ! ( Number(elem) ) ) { throw new Error('Ошибка ввода') }
-                else {
-                    stack.push(elem)
-                }
+
+pushElementsOnStack = () => { // добавляем элементы в стек
+
+    try {
+        let N = prompt('Сколько элементов вы хотите добавить в стек:','')
+        if ( ! ( Number(N) ) ) { throw new Error('Ошибка ввода') }
+        for (let i = 0 ; i < N; i++){
+            let elem = parseFloat(prompt('Элемент ' + (i+1) + ':',''))
+            if ( ! ( Number(elem) ) ) { throw new Error('Ошибка ввода') }
+            else {
+                stack.push(elem)
             }
-        }catch (e) {
-            alert(e)
         }
-    })
+    }catch (e) {
+        alert(e)
+    }
 
 }
+
+
 deleteLastElementFromStack = () => { //удаляем последний ( или "верхний") элемент из стека
-    let deleteLastElement = document.querySelector('#btn2')
-    deleteLastElement.addEventListener('click',function () {
     stack.pop()
-    })
 }
+
 
 saveStackInLocalStorage = () => { // можем сохранить наш стек в local storage браузера
-    let saveStack = document.querySelector('#btn3')
-    saveStack.addEventListener('click',function () {
         let json = JSON.stringify(stack)
         localStorage.setItem('stack',json)
-    })
 }
-pushElementOnStack()
-deleteLastElementFromStack()
-saveStackInLocalStorage()
+
+
+
+let addElements = document.querySelector('#btn1')
+addElements.addEventListener('click',pushElementsOnStack)//при нажатии на кнопку Добавиить элемент добавим элементы в стек
+
+let deleteLastElement = document.querySelector('#btn2')
+deleteLastElement.addEventListener('click',deleteLastElementFromStack)//при нажатии на кнопку Удалить элемент
+// удалим последний элемент из стека
+
+let saveStack = document.querySelector('#btn3')
+saveStack.addEventListener('click',saveStackInLocalStorage)//при нажатии на кнопку Сохранить сохраним стек в local
+//storage браузера
