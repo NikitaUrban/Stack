97625250a1130cd487c8bf9764e43252d8ca56d3
nikitka <nick.urbanovich@mail.ru>
2020-07-28 12:04:19 +0300

    successfull try/catch in function add and code of saving stack

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 let stack = []
-pushElementOnStack = () => {
+pushElementOnStack = () => { // добавляем элементы в стек
     let addElements=document.querySelector('#btn1')
     addElements.addEventListener('click',function () {
-        let N = prompt('Сколько элементов вы хотите добавить в стек:','')
-        for (let i = 0 ; i < N; i++){
-            let elem = parseFloat(prompt('Элемент '+(i+1)+':',''))
-            stack.push(elem)
+        try {
+            let N = prompt('Сколько элементов вы хотите добавить в стек:','')
+            if ( ! ( Number(N) ) ){
+                throw new Error('Ошибка ввода')
+            }
+            for (let i = 0 ; i < N; i++){
+                let elem = parseFloat(prompt('Элемент ' + (i+1) + ':',''))
+                if ( ! ( Number(elem) ) ) { throw new Error('Ошибка ввода') }
+                else {
+                    stack.push(elem)
+                }
+            }
+        }catch (e) {
+            alert(e)
         }
     })
 
 }
-deleteLastElementFromStack = () => {
+deleteLastElementFromStack = () => { //удаляем последний ( или "верхний") элемент из стека
     let deleteLastElement = document.querySelector('#btn2')
     deleteLastElement.addEventListener('click',function () {
     for (let i = 0 ; i < stack.length ; i++){
@@ -20,5 +30,14 @@ deleteLastElementFromStack = () => {
     }
     })
 }
+
+saveStackInLocalStorage = () => { // можем сохранить наш стек в local storage браузера
+    let saveStack = document.querySelector('#btn3')
+    saveStack.addEventListener('click',function () {
+        let json = JSON.stringify(stack)
+        localStorage.setItem('stack',json)
+    })
+}
 pushElementOnStack()
 deleteLastElementFromStack()
+saveStackInLocalStorage()
