034e07a499e504d53a9d113d9ec7ed8d30513025
nikitka <nick.urbanovich@mail.ru>
2020-07-28 11:17:02 +0300

    good work of delete and add methods

---
+++ script.js
@@ -0,0 +1,24 @@
+let stack = []
+pushElementOnStack = () => {
+    let addElements=document.querySelector('#btn1')
+    addElements.addEventListener('click',function () {
+        let N = prompt('Сколько элементов вы хотите добавить в стек:','')
+        for (let i = 0 ; i < N; i++){
+            let elem = parseFloat(prompt('Элемент 1:',''))
+            stack.push(elem)
+        }
+    })
+
+}
+deleteLastElementFromStack = () => {
+    let deleteLastElement = document.querySelector('#btn2')
+    deleteLastElement.addEventListener('click',function () {
+    for (let i = 0 ; i < stack.length ; i++){
+        if (i === stack.length-1){
+            stack.pop(stack[i])
+        }
+    }
+    })
+}
+pushElementOnStack()
+deleteLastElementFromStack()
